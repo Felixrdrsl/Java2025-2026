e5d5f92c34f775e443972e435baad41f0d0b98f9
felixrdrsl <felixrdrsl@gmail.com>
2025-10-06 17:33:22 +0200

    e
    Signed-off-by: felixrdrsl <felixrdrsl@gmail.com>

---
+++ src/tema1/EjercicioClasePractica9.java
@@ -0,0 +1,24 @@
+package tema1;
+
+public class EjercicioClasePractica9 {
+    public static void main(String[] args) {
+
+
+        double x1,y1,x2,y2;
+        double distancia;
+
+
+        x1 = 12.5;
+        x2 = 18.34;
+
+        y1 = 14.2;
+        y2 = 11.23;
+
+        distancia = Math.sqrt(Math.pow ((x2-x1),2) + Math.pow ((y2-y1),2));
+        System.out.println("La distancia es: " + distancia);
+
+
+
+
+    }
+}
---
+++ src/tema1/EjercicioPracticaClase11.java
@@ -0,0 +1,19 @@
+package tema1;
+
+public class EjercicioPracticaClase11 {
+    public static void main(String[] args) {
+
+        int numero = 19;
+        int resultado; // 91
+
+        int dig1 = numero / 10;
+        int digt2 = numero % 10;
+
+        resultado = (digt2 * 10 ) + dig1;
+        System.out.println(resultado);
+
+
+
+
+    }
+}
