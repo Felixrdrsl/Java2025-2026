6f630fd6319efaabdaff21fea5d9af4610b946dc
felixrdrsl <felixrdrsl@gmail.com>
2025-10-06 17:40:20 +0200

    e
    Signed-off-by: felixrdrsl <felixrdrsl@gmail.com>

---
+++ src/tema1/EjercicioClasePracticas13.java
@@ -0,0 +1,17 @@
+package tema1;
+
+public class EjercicioClasePracticas13 {
+    public static void main(String[] args) {
+
+
+        int Correctas = 10;
+        int Incorrecta = 3;
+        int enBlanco = 7;
+
+        int nota =( 5 * Correctas) + (Incorrecta * -1);
+        System.out.println(nota);
+
+
+
+    }
+}
