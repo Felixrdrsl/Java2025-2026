3e9142b3e6fb8e4a9b9804838c31be52466aec6b
felixrdrsl <felixrdrsl@gmail.com>
2026-01-12 18:00:14 +0100

    eee
    Signed-off-by: felixrdrsl <felixrdrsl@gmail.com>

---
+++ src/tema4/ejemplos/TestCoversosDivisas.java
@@ -0,0 +1,14 @@
+package tema4.ejemplos;
+
+public class TestCoversosDivisas {
+    public static void main(String[] args) {
+
+        System.out.println(ConversorDivisas.conversorEuroToDolar(550.0));
+        System.out.println(ConversorDivisas.conversorDolarToEuro(1199.0));
+        System.out.println(ConversorDivisas.conversorEuroToLibra(1000.0));
+        System.out.println(ConversorDivisas.conversorRenminbiToEuro(10000.0));
+
+
+
+    }
+}
