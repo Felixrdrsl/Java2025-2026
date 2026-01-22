0c189c7481ebf3efd8e9e03f4c43a67c4fc4cd45
felixrdrsl <felixrdrsl@gmail.com>
2026-01-22 17:20:20 +0100

    nuevo
    Signed-off-by: felix <felixrdrsl@gmail.com>

--- src/tema5/ejercicioClase/modelos/PrioridadTarea.java
+++ src/tema5/ejercicioClase/modelos/PrioridadTarea.java
@@ -1,9 +1,18 @@
 package tema5.ejercicioClase.modelos;
 
 public enum PrioridadTarea {
-    BAJA,
-    MEDIA,
-    ALTA,
-    URGENTE
+    BAJA(1),
+    MEDIA(2),
+    ALTA(2),
+    URGENTE(4);
+
+    private int prioridad;
+        PrioridadTarea(int valor){
+            this.prioridad = valor;
+        }
+
+        public int getValor(){
+            return prioridad;
+        }
 
 }
