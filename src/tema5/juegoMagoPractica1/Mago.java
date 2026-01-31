40829445b327b6cbec51e71e6ffd92e798690788
felix <felixrdrsl@gmail.com>
2026-01-31 18:04:38 +0100

    nuevo
    Signed-off-by: felix <felixrdrsl@gmail.com>

--- src/tema5/juegoMagoPractica1/Mago.java
+++ src/tema5/juegoMagoPractica1/Mago.java
@@ -69,12 +69,6 @@ public class Mago {
             IO.println("No tienes la energia necesaria");
             return;
         }
-
-        //Echale un vistazo a esto porque me sale a veces negativo y no se si es por el orden
-        //o porque he hecho yo otra cosa mal
-        energia -= hechizo.getEnergiaNecesaria();
-        IO.println("Lanzar " + nombreHechizo + " Energia restante: " + energia);
-
         if (hechizo.esEfectivo(prueba)){
             energia += prueba.getRecompensa();
             IO.println("Has superado la prueba " + prueba.getRecompensa() + " de energia ganada");
@@ -84,9 +78,17 @@ public class Mago {
                     + prueba.getNivelDificultad());
             IO.println("Niveles de los hechizos aprendidos = " + getHechizos());
         }
+
+        //He intentado poner este metodo como para avisar que no tiene energia
+        //Pero al no tener energia suficiente no soy capaz de hacer que pierda lo que tiene porque no lo usa,
+        //entonces me he quedado sin ideas de como solucionar eso.
         if (energia <= 0) {
             IO.println("Te has quedado sin energía");
         }
+        //Echale un vistazo a esto porque me sale a veces negativo y no se si es por el orden
+        //o porque he hecho yo otra cosa mal
+        energia -= hechizo.getEnergiaNecesaria();
+        IO.println("Lanzar " + nombreHechizo + " Energia restante: " + energia);
     }
     public Hechizo buscar(String nombreHechizo){
         for (Hechizo hechizo: hechizos){
